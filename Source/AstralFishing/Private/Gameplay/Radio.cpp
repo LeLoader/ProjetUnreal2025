9c3684b514d45a2e8b66ab8799bbc9840be38237
LeLoader <61639484+LeLoader@users.noreply.github.com>
2026-01-12 02:05:09 +0100

    Added the Radio in cpp

---
+++ Source/AstralFishing/Private/Gameplay/Radio.cpp
@@ -0,0 +1,37 @@
+
+
+
+#include "Gameplay/Radio.h"
+
+#include "Components/AudioComponent.h"
+#include "AudioParameterControllerInterface.h"
+
+ARadio::ARadio()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bIsToggleInteraction = false;
+
+	RadioMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SM Radio"));
+	RootComponent = RadioMesh;
+	ToOutline.Add(RadioMesh);
+
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("MusicComponent"));
+	AudioComponent->SetupAttachment(RadioMesh);
+}
+
+void ARadio::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AudioComponent->SetSound(Music);
+	AudioComponent->Play();
+}
+
+FInteractionResult ARadio::Interact(UInteractionComponent* InteractionSource)
+{
+	if (Music) {
+		AudioComponent->SetTriggerParameter(FName("NextSound"));
+		return FInteractionResult(true, bIsToggleInteraction);
+	}
+	return FInteractionResult(false, bIsToggleInteraction);
+}
---
+++ Source/AstralFishing/Public/Gameplay/Radio.h
@@ -0,0 +1,46 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/Interactable.h"
+#include "Gameplay/InteractableBase.h"
+
+#include "Radio.generated.h"
+
+class UAudioComponent;
+
+UCLASS()
+class ASTRALFISHING_API ARadio : public AInteractableBase
+{
+	GENERATED_BODY()
+	
+public:	
+	ARadio();
+
+protected:
+	virtual void BeginPlay() override;
+
+#pragma region Components
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> RadioMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAudioComponent> AudioComponent;
+
+#pragma endregion Components
+
+#pragma region Interaction Implementation
+
+public:
+	FInteractionResult Interact(UInteractionComponent* InteractionSource) override;
+
+#pragma endregion Interaction Implementation
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<USoundBase> Music;
+};
