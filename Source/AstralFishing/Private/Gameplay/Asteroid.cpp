15698e3cd3e1862a5a26420810a6b5cb63a34499
LeLoader <61639484+LeLoader@users.noreply.github.com>
2026-04-12 03:29:27 +0200

    Added a random rotation to Asteroids

--- Source/AstralFishing/Private/Gameplay/Asteroid.cpp
+++ Source/AstralFishing/Private/Gameplay/Asteroid.cpp
@@ -4,6 +4,7 @@
 #include "Gameplay/Asteroid.h"
 
 #include "GameFramework/RotatingMovementComponent.h"
+#include <Kismet/KismetMathLibrary.h>
 
 
 // Sets default values
@@ -15,4 +16,7 @@ AAsteroid::AAsteroid()
 	RootComponent = MeshComponent;
 
 	RotatingComponent = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("RotatingComponent"));
+	FRotator RandomRotator = UKismetMathLibrary::RandomRotator(false);
+	FRotator ScaledDownRandomRotator = FRotator(RandomRotator.Pitch / 180, RandomRotator.Yaw / 180, RandomRotator.Roll / 180);
+	RotatingComponent->RotationRate = ScaledDownRandomRotator;
 }
